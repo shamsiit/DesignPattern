5209cdd20a6451d3f656377c86c90f063a0c3790
Shahriar Mohammad <shahriar@ipvisionsoft.com>
2017-11-11 10:56:30 +0600

    factory pattern

---
+++ src/factorypattern/Main.java
@@ -0,0 +1,46 @@
+package factorypattern;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Create the factory object
+		EnemyShipFactory shipFactory = new EnemyShipFactory();
+
+		// Enemy ship object
+
+		EnemyShip theEnemy = null;
+
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.print("What type of ship? (U / R / B)");
+
+		if (userInput.hasNextLine()) {
+
+			String typeOfShip = userInput.nextLine();
+
+			theEnemy = shipFactory.makeEnemyShip(typeOfShip);
+
+			if (theEnemy != null) {
+
+				doStuffEnemy(theEnemy);
+
+			} else
+				System.out.print("Please enter U, R, or B next time");
+
+		}
+	}
+
+	public static void doStuffEnemy(EnemyShip anEnemyShip) {
+
+		anEnemyShip.displayEnemyShip();
+
+		anEnemyShip.followHeroShip();
+
+		anEnemyShip.enemyShipShoots();
+
+	}
+
+}
